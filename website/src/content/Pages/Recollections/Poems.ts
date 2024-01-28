85bd11d70beb8fbb37fa6fd400c0b175b621554e
unknown <kontogiannisfilippos99@gmail.com>
2024-01-28 19:19:01 +0000

    Reverting

--- website/src/content/Pages/Recollections/Poems.ts
+++ website/src/content/Pages/Recollections/Poems.ts
@@ -11,13 +11,13 @@ const extractPoems = async (): Promise<PoemProps[]> => {
   return await fetch('/Poems.txt')
     .then(async (r) => await r.text())
     .then((text) => {
-      const p = text.split('-----------------');
+      const p = text.split('-----------------\n'); // Include \n for UNIX
       // console.log(p);
       const poems = [];
       for (let i = 0; i < p.length - 1; i++) {
         
 
-        const lines = p[i].split('\r\n'); // For windows \r\n for UNIX: \n
+        const lines = p[i].split('\n'); // For windows \r\n for UNIX: \n
         
         const verses = p[i].substring(p[i].indexOf('\n'), p[i].length - 1);
         poems.push({ title: lines[1], verses });
