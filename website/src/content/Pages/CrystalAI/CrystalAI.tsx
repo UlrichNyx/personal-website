ea6f221095c509959ef94ca7cba8ae880d1d3a54
unknown <kontogiannisfilippos99@gmail.com>
2024-03-21 18:26:32 +0000

    Started work on the CrystalAI section

--- website/src/content/Pages/CrystalAI/CrystalAI.tsx
+++ website/src/content/Pages/CrystalAI/CrystalAI.tsx
@@ -1,15 +1,23 @@
+import { Typography } from '@mui/material';
+import Screenshot from '../../../assets/crystalai/screenshot.png';
 
 const content = {
-  description:'',
+  description:'This is a project that I had been wanting to do for a while now. I drew inspiration from the multitude of YouTube videos where an AI algorithm is trained to beat a certain video game.',
   html: (
     <div
       style={{
         display: 'flex',
         flexDirection:'column',
-        gap:'2vh',
+        gap:'5vh',
+        marginTop:'5vh',
         justifyContent: 'center',
       }}
     >
+      <Typography>This is currently a project in the making, so I will make sure to update it as progress goes along!</Typography>
+      <Typography>Currently, I have managed to make an interface with the emulator using pyboy.</Typography>
+      <Typography>The bot can now move and interact randomly through the space!</Typography>
+      <img src={Screenshot} style={{borderRadius:4}}/>
+      <Typography>As you can see it has not yet managed to leave the room yet :P (but it got close!)</Typography>
     </div>
   ),
 };
