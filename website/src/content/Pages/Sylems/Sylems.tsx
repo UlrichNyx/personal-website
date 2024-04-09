3767d75357556eb2358931576525472e574f61e1
unknown <kontogiannisfilippos99@gmail.com>
2024-04-09 18:10:49 +0100

    Changed vereselyum section

--- website/src/content/Pages/Sylems/Sylems.tsx
+++ website/src/content/Pages/Sylems/Sylems.tsx
@@ -21,33 +21,14 @@ const content = {
       <Typography>Hey! This page is currently under construction! :3</Typography>
       <img src={cat} style={{ borderRadius: 4, width: 200 }}></img>
       <Typography>(Imagine the cat is the content for now)</Typography>
-      <Typography>Please take a look at our projects below:</Typography>
-      <Button
-        variant='contained'
-        style={{
-          color: Colors.white,
-          backgroundColor: Colors.sylemsBlue,
-          textTransform: 'none',
-          alignSelf: 'center',
-        }}
-        endIcon={<ArrowForwardIosIcon />}
-        onClick={() => window.open('https://itch.io/jam/pirate/rate/2491773')}
-      >
-        Fluffy Snuggly Adventures!
-      </Button>
-      <Button
-        variant='contained'
-        style={{
-          color: Colors.white,
-          backgroundColor: Colors.blurple,
-          textTransform: 'none',
-          alignSelf: 'center',
-        }}
-        endIcon={<ArrowForwardIosIcon />}
-        onClick={() => window.open('https://itch.io/jam/pirate/rate/2491773')}
-      >
-        Project AetherGate
-      </Button>
+      <Typography>
+        In the near future I will share some recent projects we have been working on :))
+      </Typography>
+      <Typography>
+        In the more distant future I will also make sure to create a dedicated website for
+        Vereselyum but as we are still in development, that will have to wait for now.{' '}
+      </Typography>
+
       <Typography>
         Here is our submission in the Pirate Software GameJam hosted in January 2024:
       </Typography>
@@ -77,12 +58,62 @@ const content = {
           here.
         </strong>
       </Typography>
+      <Typography>
+        {' '}
+        In my Game Design and Development module in uni, I got the chance to develop two game
+        prototypes which my teaching staff also got to playtest.
+      </Typography>
+      <Typography style={{ fontWeight: 'bold' }}>Nightly Ride to Nasyne</Typography>
+      <iframe
+        width='560'
+        height='315'
+        src='https://www.youtube.com/embed/sJn1n7DsaBE?si=DBQi0PYwy1mWGdX9'
+        title='YouTube video player'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        referrerPolicy='strict-origin-when-cross-origin'
+        allowFullScreen
+      ></iframe>
+      <Typography style={{ fontWeight: 'bold' }}>Horseman&#39;s Lair</Typography>
+      <iframe
+        width='560'
+        height='315'
+        src='https://www.youtube.com/embed/cnyCBJMRvqE?si=jQQhm9ziZVOxJma3&amp;start=1913'
+        title='YouTube video player'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        referrerPolicy='strict-origin-when-cross-origin'
+        allowFullScreen
+      ></iframe>
     </div>
   ),
 };
 
 /*
-
+      <Button
+        variant='contained'
+        style={{
+          color: Colors.white,
+          backgroundColor: Colors.sylemsBlue,
+          textTransform: 'none',
+          alignSelf: 'center',
+        }}
+        endIcon={<ArrowForwardIosIcon />}
+        onClick={() => window.open('https://itch.io/jam/pirate/rate/2491773')}
+      >
+        Fluffy Snuggly Adventures!
+      </Button>
+      <Button
+        variant='contained'
+        style={{
+          color: Colors.white,
+          backgroundColor: Colors.blurple,
+          textTransform: 'none',
+          alignSelf: 'center',
+        }}
+        endIcon={<ArrowForwardIosIcon />}
+        onClick={() => window.open('https://itch.io/jam/pirate/rate/2491773')}
+      >
+        Project AetherGate
+      </Button>
 
 
 */
